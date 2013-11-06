a593e80732ea214e419702dadb17aedda6bc1157
Luke Barlow <luke.barlow@gmail.com>
2013-11-06 12:07:39 +0000

    adding wemix

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express'),
     app = express(),
     rockpool = require('../rockpool'),
+    wemix = require('../wemix'),
     prong = require('../prong'),
     fours = require('../fours')
     port = 8082;
@@ -8,7 +9,13 @@ var express = require('express'),
 app.use('/rockpool', rockpool);
 app.use('/fours', fours);
 app.use('/prong', prong);
+app.use('/wemix', wemix);
 app.use(express.static(__dirname + '/public'));
 var server = app.listen(port);
-rockpool.initSocket(server);
+
+// the socket stuff
+var io = require('socket.io').listen(server, { log: false });
+rockpool.initSocket(io);
+wemix.initSocket(io);
+
 console.log('Fork website running at localhost:' + port);
