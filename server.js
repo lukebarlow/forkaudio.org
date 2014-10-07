d928440b656fd9c43d2aca827516316e160fa05f
Luke Barlow <luke.barlow@gmail.com>
2014-10-07 04:46:18 +0100

    removing rockpool for now, pending improvements

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 var express = require('express'),
     app = express(),
-    rockpool = require('../rockpool'),
+    
+    //rockpool = require('../rockpool'),
     wemix = require('../wemix'),
     prong = require('../prong'),
+    browserify = require('browserify-middleware'),
+    coffeeify = require('caching-coffeeify'),
     //fours = require('../fours')
     port = 8081;
 
-app.use('/rockpool', rockpool);
+browserify.settings('extensions', ['.coffee','.js'])
+
+//app.use('/rockpool', rockpool);
 //app.use('/fours', fours);
 app.use('/prong', prong);
 app.use('/wemix', wemix);
@@ -15,7 +20,7 @@ var server = app.listen(port);
 
 // the socket stuff
 var io = require('socket.io').listen(server, { log: false });
-rockpool.initSocket(io);
+//rockpool.initSocket(io);
 wemix.initSocket(io);
 
 console.log('Fork website running at localhost:' + port);
